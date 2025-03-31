d8e31416274939d3ba7361acd341aecd5ca13548
Ldaz54 <ldaz@mail.austral.edu.ar>
2025-03-31 11:05:19 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,12 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = first_name + ' ' + last_name
+    upper_name = full_name.upper()
+    print(upper_name)
+    title_name = full_name.title()
+    print(title_name)
+    lower_name = full_name.lower()
+    print(lower_name)
+
+ada()
