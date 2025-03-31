4b25726efbba324f72fe2cded2ce681f3cf2b72b
Ldaz54 <ldaz@mail.austral.edu.ar>
2025-03-31 11:19:20 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -2,11 +2,11 @@ def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
     full_name = first_name + ' ' + last_name
-    upper_name = full_name.upper()
-    print(upper_name)
-    title_name = full_name.title()
-    print(title_name)
     lower_name = full_name.lower()
     print(lower_name)
-
+    title_name = full_name.title()
+    print(title_name)
+    upper_name = full_name.upper()
+    print(upper_name)
+    print(f"\t{lower_name}")
 ada()
