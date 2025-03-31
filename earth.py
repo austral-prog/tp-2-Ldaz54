d11e36efaeaeea9c32e8b5b470b5f94b7355afe8
Ldaz54 <ldaz@mail.austral.edu.ar>
2025-03-31 12:19:47 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,6 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    booleano_1 = x > y
+    booleano_2 = x < y
+    print(f"The result of {x} comes first in the dictionary than {y} is {booleano_2}.\nThe result of {y} comes first in the dictionary than {x} is {booleano_1}.")
